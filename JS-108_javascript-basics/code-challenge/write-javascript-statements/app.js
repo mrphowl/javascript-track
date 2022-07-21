ca815c7db7f5088cc95f1d8f1b2806f16124c6f5
Paul Adraneda <pegadraneda@gmail.com>
2022-07-21 18:13:10 +0800

    Code challenge solution

---
+++ JS-108_javascript-basics/code-challenge/write-javascript-statements/app.js
@@ -0,0 +1,6 @@
+// Print "Begin program" in the console.
+console.log('Begin program');
+// Show a message in the browser "I am Programming!".
+alert('I am Programming!');
+// Print "End program" in the console.
+console.log('End program');
