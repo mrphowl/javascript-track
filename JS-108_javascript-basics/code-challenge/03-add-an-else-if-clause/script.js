63c4320bb4d3800528a7dcc30e438c419cb76184
Paul Adraneda <pegadraneda@gmail.com>
2022-07-22 19:16:04 +0800

    Code challenge: Add an 'else if' clause

---
+++ JS-108_javascript-basics/code-challenge/03-add-an-else-if-clause/script.js
@@ -0,0 +1,12 @@
+// Code challenge: Add an `else if` Clause
+// Add an else if clause that tests if the isStudent variable is true.
+// If it's true, set the value of the message variable to "Welcome student".
+const isAdmin = false;
+const isStudent = true;
+let message;
+
+if ( isAdmin ) {
+  message = 'Welcome admin';
+} else if ( isStudent ) {
+  message = 'Welcome student'
+}
