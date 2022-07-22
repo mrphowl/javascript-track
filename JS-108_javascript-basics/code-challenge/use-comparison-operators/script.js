73a7162d701d2d44f0264c4137eb6165143ab5de
Paul Adraneda <pegadraneda@gmail.com>
2022-07-22 17:34:44 +0800

    Code challenge: Use Comparison Operators

---
+++ JS-108_javascript-basics/code-challenge/use-comparison-operators/script.js
@@ -0,0 +1,14 @@
+// Code challenge: Use Comparison Operators
+// Add a conditional statement that tests if the value in variable a is greater
+// than the value in variable b. If it is, log the message "a is greater than
+// b" to the console. Then, add an else clause that logs the message "a is not
+// greater than b".
+const a = 10;
+const b = 20;
+const c = 30;
+
+if ( a > b ) {
+  console.log('a is greater than b');
+} else {
+  console.log('a is not greater than b');
+}
