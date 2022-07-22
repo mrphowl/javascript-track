43f1026d454a66e46f731386a388a8ec46b623aa
Paul Adraneda <pegadraneda@gmail.com>
2022-07-22 19:17:55 +0800

    Exercise: Guess the Number game

---
+++ JS-108_javascript-basics/exercise/guess-the-number/js/app.js
@@ -0,0 +1,29 @@
+// Guess the number
+let guesses = 0;
+let answer;
+let number;
+let gameOver = false;
+
+alert('I am thinking of a number from 1 to 20.');
+
+number = Math.ceil(Math.random() * 20);
+console.log(`number: ${number}`);
+
+while ( ! gameOver && guesses < 3 ) {
+  answer = prompt("Guess the number");
+  guesses++;
+
+  console.log(`${number}, ${answer}`);
+
+  if ( +answer === number ) {
+    alert('You got it!');
+    gameOver = true;
+  } else {
+    if ( guesses === 3 ) {
+      alert(`Sorry, the number is ${number}.`);
+    } else {
+      alert('Sorry, try again.');
+      answer = undefined;
+    }
+  }
+}
