c7492f7c52bd238590e12d2f7251e7194e9f52d6
Paul Adraneda <pegadraneda@gmail.com>
2022-07-25 15:45:21 +0800

    Switch the code blocks in the if statement. Changed the if condition.

--- JS-109_javascript-numbers/code-challenge/random-number-challenge/js/random.js
+++ JS-109_javascript-numbers/code-challenge/random-number-challenge/js/random.js
@@ -10,13 +10,14 @@ const input = prompt('Please input a number greater than 1.');
 // Convert the input to a number
 const numRange = parseInt(input);
 
-if ( numRange < 2) {
-  main.innerHTML = 'The input must be a number greater than 1. Refresh the page to try again.';
-} else {
+if ( numRange && numRange > 1) {
   // Use Math.random() and the user's number to generate a random number
   const randomNumber = Math.floor(Math.random() * numRange) + 1;
 
   // Create a message displaying the random number
   console.log(`${randomNumber} is a number between 1 and ${numRange}.`);
   main.innerHTML = `<p>${randomNumber} is a number between 1 and ${numRange}.</p>`;
+
+} else {
+    main.innerHTML = 'The input must be a number greater than 1. Refresh the page to try again.';
 }
