3679f3be7dc128c75e53e5eae9c7a9186a0d1202
Paul Adraneda <pegadraneda@gmail.com>
2022-07-25 14:08:02 +0800

    Code challenge: Storing numbers in a variable

---
+++ JS-108_javascript-basics/code-challenge/storing-numbers-in-variables/script.js
@@ -0,0 +1,8 @@
+/* JS-109: JavaScript Numbers - Working with Numbers */
+/*
+Code Challenge: Storing Numbers In Variables
+*/
+// Task 1: Create a variable named age and assign it your age.
+const age = 37;
+// Task 2: Add a second variable named price and assign it a floating point value (a number with a decimal).
+const price = 99.99;
