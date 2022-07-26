62ed5235246336be6c9c15ac96c80509364995f9
Paul Adraneda <pegadraneda@gmail.com>
2022-07-26 21:45:06 +0800

    Removed isNumeric() function and changed condition for checking the argument types.

--- JS-110_javascript-functions/code-challenge/function-challenge/js/random.js
+++ JS-110_javascript-functions/code-challenge/function-challenge/js/random.js
@@ -5,13 +5,6 @@
  */
 const main = document.querySelector('main');
 
-function isNumeric(arg) {
-  if ( typeof arg === 'number' ) {
-    return true;
-  }
-  return false;
-}
-
 /**
  * Returns a random number between two numbers.
  *
@@ -20,33 +13,25 @@ function isNumeric(arg) {
  * @return {number} The random number value.
  */
 function randomNumber(lower, upper) {
-  if ( isNumeric(lower) && isNumeric(upper) && !isNaN(lower) && !isNaN(upper) ) {
-    return Math.floor(Math.random() * (upper - lower + 1)) + lower;
-  } else {
-    console.log('All arguments must be a number.');
-    return false;
+  if ( isNaN(lower) || isNaN(upper) ) {
+    throw Error('All arguments must be a number.');
   }
+  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
 }
 
 // Call the function and pass it different values
 let lower = 1;
 let upper = 6;
-let randNum = randomNumber(lower, upper);
-let mes = `<p>${randNum} is a number between ${lower} and ${upper}.</p>`;
-
-main.innerHTML += mes;
-console.log(mes);
+main.innerHTML += `<p>${randomNumber(lower, upper)} is a number between ${lower} and ${upper}.</p>`;
 
 lower = 0;
 upper = 100;
 main.innerHTML += `<p>${randomNumber(lower, upper)} is a number between ${lower} and ${upper}.</p>`;
 
-lower = 5;
+lower = 'five';
 upper = 67;
-randNum = randomNumber(lower, upper);
+main.innerHTML += `<p>${randomNumber(lower, upper)} is a number between ${lower} and ${upper}.</p>`;
 
-if (randNum !== false && randNum !== NaN) {
-  main.innerHTML += `<p>${randNum} is a number between ${lower} and ${upper}.</p>`;
-} else {
-  main.innerHTML += `<p>All arguments must be a numeric value. Input: ${lower} and ${upper}</p>`
-}
+lower = -4;
+upper = 67;
+main.innerHTML += `<p>${randomNumber(lower, upper)} is a number between ${lower} and ${upper}.</p>`;
