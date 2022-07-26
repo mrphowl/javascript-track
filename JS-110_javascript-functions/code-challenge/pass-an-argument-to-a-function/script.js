84d90dbd623adf56a546738d89c02cb70cf65d70
Paul Adraneda <pegadraneda@gmail.com>
2022-07-26 18:05:16 +0800

    Code challenge: Pass an argument to a function

---
+++ JS-110_javascript-functions/code-challenge/pass-an-argument-to-a-function/script.js
@@ -0,0 +1,9 @@
+/* JS-110: JavaScript Functions
+Code challenge: Pass an Argument to a Function
+*/
+// Task 1: Create a function named returnValue.
+// Task 2: Modify the returnValue function to accept one argument named value.
+function returnValue(value) {
+  // Task 3: Set the returnValue function to return the value stored in the value parameter.
+  return value;
+}
