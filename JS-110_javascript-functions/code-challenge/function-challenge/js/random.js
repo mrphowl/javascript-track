785ecf258c2fbb942673928a42e6f3d1133a8f51
Paul Adraneda <pegadraneda@gmail.com>
2022-07-26 20:24:45 +0800

    Added checking on arguments passed. Should accept number that are not NaN.

--- JS-110_javascript-functions/code-challenge/function-challenge/js/random.js
+++ JS-110_javascript-functions/code-challenge/function-challenge/js/random.js
@@ -4,8 +4,13 @@
  * Code challenge: Function challenge
  */
 const main = document.querySelector('main');
-const lower = 1;
-const upper = 6;
+
+function isNumeric(arg) {
+  if ( typeof arg === 'number' ) {
+    return true;
+  }
+  return false;
+}
 
 /**
  * Returns a random number between two numbers.
@@ -15,13 +20,33 @@ const upper = 6;
  * @return {number} The random number value.
  */
 function randomNumber(lower, upper) {
-  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+  if ( isNumeric(lower) && isNumeric(upper) && !isNaN(lower) && !isNaN(upper) ) {
+    return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+  } else {
+    console.log('All arguments must be a number.');
+    return false;
+  }
 }
 
 // Call the function and pass it different values
-const randNum = randomNumber(lower, upper);
+let lower = 1;
+let upper = 6;
+let randNum = randomNumber(lower, upper);
+let mes = `<p>${randNum} is a number between ${lower} and ${upper}.</p>`;
 
-// Output
-const mes = `<p>${randNum} is a number between ${lower} and ${upper}.</p>`;
-main.innerHTML = mes;
+main.innerHTML += mes;
 console.log(mes);
+
+lower = 0;
+upper = 100;
+main.innerHTML += `<p>${randomNumber(lower, upper)} is a number between ${lower} and ${upper}.</p>`;
+
+lower = 5;
+upper = 67;
+randNum = randomNumber(lower, upper);
+
+if (randNum !== false && randNum !== NaN) {
+  main.innerHTML += `<p>${randNum} is a number between ${lower} and ${upper}.</p>`;
+} else {
+  main.innerHTML += `<p>All arguments must be a numeric value. Input: ${lower} and ${upper}</p>`
+}
