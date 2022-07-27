9690013aaa1e4344d891bb481a6d60471d310890
Paul Adraneda <pegadraneda@gmail.com>
2022-07-27 16:28:01 +0800

    Code challenge: Create a For loop

---
+++ JS-111_javascript-loops/working-with-for-loops/code-challenge/create-a-for-loop/script.js
@@ -0,0 +1,9 @@
+/** JS-111: JavaScript Loops
+ * Code challenge: Create a For Loop
+ * 
+ * Task 1: Create a for loop that logs the numbers 5 to 100 to the console.
+ * Use the console.log() method to log a value to the console.
+ */
+for ( let i = 5; i <= 100; i++ ) {
+  console.log(i);
+}
