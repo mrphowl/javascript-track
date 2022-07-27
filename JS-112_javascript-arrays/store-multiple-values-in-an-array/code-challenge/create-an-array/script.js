502b7162f5fc3375af5ca2c8f53ffb62d766cb06
Paul Adraneda <pegadraneda@gmail.com>
2022-07-27 22:45:23 +0800

    Code challenge: Create an array

---
+++ JS-112_javascript-arrays/store-multiple-values-in-an-array/code-challenge/create-an-array/script.js
@@ -0,0 +1,10 @@
+/**
+ * JS-112: JavaScript Arrays - Store Multiple Values In An Array
+ * 
+ * Code challenge: Create An Array
+ */
+// Task 1: Assign an array to a variable named data and store
+// any three numbers inside it.
+const data = [1, 2, 3];
+// Task 2: Below the data array, declare a second variable named assorted and assign it an array. Inside the new array, add one string value, a number, and a Boolean (like true or false). The assorted array should have three values in total.
+const assorted = ['zero', 0, false];
