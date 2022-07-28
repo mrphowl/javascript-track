7b5c9c66dda0b4a4a2f21987bd941c52662c04c6
Paul Adraneda <pegadraneda@gmail.com>
2022-07-28 18:14:13 +0800

    Code challenge: Access and Set Object Properties

---
+++ JS-113_javascript-objects/object-basics/access-and-set-object-properties/script.js
@@ -0,0 +1,14 @@
+/**
+ * JS-112: JavaScript Arrays - Loop Through Arrays
+ * 
+ * Code challenge: Access and Set Object Properties
+ */
+const addy = {
+  animal: 'dog', 
+  age: 3,
+  breed: 'pug'
+};
+// Task 1: Add a line of code that sets the age property to 4. Don't change the original declaration of the object.
+addy.age = 4;
+// Task 2: Add a new property named isFriendly and set its value to true.
+addy.isFriendly = true;
