a11280de458c2042d5afe4d1f973313a5d3abdab
Paul Adraneda <pegadraneda@gmail.com>
2022-07-28 15:00:02 +0800

    Code challenge: Loop Through An Array

---
+++ JS-112_javascript-arrays/loop-through-arrays/code-challenge/loop-through-an-array/script.js
@@ -0,0 +1,15 @@
+/**
+ * JS-112: JavaScript Arrays - Loop Through Arrays
+ * 
+ * Code challenge: Loop Through An Array
+ */
+const temperatures = [100, 90, 99, 80, 70, 65, 30, 10];
+/*
+Task 1: Use a for loop to iterate over the values inside the temperatures
+array. Start from the first element (100) to the last (10). Inside the loop,
+log the current array value to the console. Use i to represent the counter
+variable.
+*/
+for ( let i = 0; i < temperatures.length; i++ ) {
+  console.log(temperatures[i]);
+}
