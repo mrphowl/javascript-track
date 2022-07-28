d63012f29e426d8d43633ccbdcfb488ca796fe8d
Paul Adraneda <pegadraneda@gmail.com>
2022-07-28 19:16:40 +0800

    Include the folder of the image file.

--- JS-113_javascript-objects/loop-through-objects/code-challenge/build-an-object-challenge/js/pets.js
+++ JS-113_javascript-objects/loop-through-objects/code-challenge/build-an-object-challenge/js/pets.js
@@ -20,11 +20,11 @@ function createPet(name, type, breed, age, photo) {
   };
 }
 
-pet.push(createPet('Sussie', 'Dog', 'Australian Shepherd', 4, 'aussie.jpg'));
-pet.push(createPet('Hotdog', 'Dog', 'Dachshund', 5, 'dachshund.jpg'));
-pet.push(createPet('Jeff', 'Dog', 'Golden Retriever', 6, 'golden.jpg'));
-pet.push(createPet('Perry', 'Cat', 'Persian', 4, 'persian.jpg'));
-pet.push(createPet('Dude', 'Dog', 'Pug', 3, 'pug.jpg'));
-pet.push(createPet('Miming', 'Cat', 'Domestic Shorthair', 100, 'tabby.jpg'));
+pet.push(createPet('Sussie', 'Dog', 'Australian Shepherd', 4, 'img/aussie.jpg'));
+pet.push(createPet('Hotdog', 'Dog', 'Dachshund', 5, 'img/dachshund.jpg'));
+pet.push(createPet('Jeff', 'Dog', 'Golden Retriever', 6, 'img/golden.jpg'));
+pet.push(createPet('Perry', 'Cat', 'Persian', 4, 'img/persian.jpg'));
+pet.push(createPet('Dude', 'Dog', 'Pug', 3, 'img/pug.jpg'));
+pet.push(createPet('Miming', 'Cat', 'Domestic Shorthair', 100, 'img/tabby.jpg'));
 
 console.log(pet);
