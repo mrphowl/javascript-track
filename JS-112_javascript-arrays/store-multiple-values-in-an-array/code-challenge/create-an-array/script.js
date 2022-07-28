89f0091f33e1926f986a97c41a0858bbc8fc7971
Paul Adraneda <pegadraneda@gmail.com>
2022-07-28 14:42:26 +0800

    Add console.log to show the data

--- JS-112_javascript-arrays/store-multiple-values-in-an-array/code-challenge/create-an-array/script.js
+++ JS-112_javascript-arrays/store-multiple-values-in-an-array/code-challenge/create-an-array/script.js
@@ -8,3 +8,5 @@
 const data = [1, 2, 3];
 // Task 2: Below the data array, declare a second variable named assorted and assign it an array. Inside the new array, add one string value, a number, and a Boolean (like true or false). The assorted array should have three values in total.
 const assorted = ['zero', 0, false];
+
+console.log(`data: ${data}, assorted: ${assorted}`);
