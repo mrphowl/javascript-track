940dca2d9a4ae08ee8af3612b2b41c5c21fd3ff3
Paul Adraneda <pegadraneda@gmail.com>
2022-07-28 17:04:04 +0800

    Modify the code to track and list the right and wrong answers.

--- JS-112_javascript-arrays/multidimensional-arrays/code-challenge/build-a-quiz-challenge/js/quiz.js
+++ JS-112_javascript-arrays/multidimensional-arrays/code-challenge/build-a-quiz-challenge/js/quiz.js
@@ -29,6 +29,8 @@ const quizItems = [
 
 // 2. Store the number of questions answered correctly
 let correct = 0;
+const rightAnswers = [];
+const wrongAnswers = [];
 
 /* 
   3. Use a loop to cycle through each question
@@ -40,8 +42,28 @@ let correct = 0;
 for ( let i = 0; i < quizItems.length; i++ ) {
   if ( prompt(quizItems[i][0]).toLowerCase() === quizItems[i][1] ) {
     correct++;
+    rightAnswers.push(quizItems[i][0]);
+  } else {
+    wrongAnswers.push(quizItems[i][0]);
   }
 }
 
 // 4. Display the number of correct answers to the user
 document.querySelector('h1').innerHTML = `You got ${correct} correct ${correct > 1 ? 'answers' : 'answer'}.`;
+
+// Create a list item element with the given string
+let listItem = question => `<li>${question}</li>`;
+
+function createList(target, source) {
+  for ( let i = 0; i < source.length; i++ ) {
+    target.innerHTML += listItem(source[i]);
+  }
+}
+
+// List the correct answers
+const listCorrect = document.querySelector('.correct-answers');
+createList(listCorrect, rightAnswers);
+
+// List the wrong answers
+const listWrong = document.querySelector('.wrong-answers');
+createList(listWrong, wrongAnswers);
