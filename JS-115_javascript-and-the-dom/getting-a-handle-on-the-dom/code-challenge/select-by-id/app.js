5a9b09c87099e7fa90c9635a80e70bafa5bb686e
Paul Adraneda <pegadraneda@gmail.com>
2022-07-29 18:13:55 +0800

    Code challenge: Select by ID - task 2

--- JS-115_javascript-and-the-dom/getting-a-handle-on-the-dom/code-challenge/select-by-id/app.js
+++ JS-115_javascript-and-the-dom/getting-a-handle-on-the-dom/code-challenge/select-by-id/app.js
@@ -7,6 +7,10 @@
 
 // Task 1: Declare a variable named button. Assign the variable the <button> element with the ID btn-phrase. Use the method that returns an element matching the specified ID.
 const button = document.getElementById('btn-phrase');
+
+// Task 2: Declare a new variable named input. Point the variable to the input element with the ID input-phrase. Use the method that returns an element matching the specified ID.
+const input = document.getElementById('input-phrase');
+
 button.addEventListener('click', () => {
   alert(input.value);
 });
