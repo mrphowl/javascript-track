5c76c142673c17813210c31ac6aef2c7c3a60242
Paul Adraneda <pegadraneda@gmail.com>
2022-08-01 23:23:01 +0800

    Code challenge: Event Delegation

---
+++ JS-116_interacting-with-the-dom/traversing-the-dom/code-challenge/event-delegation/app.js
@@ -0,0 +1,13 @@
+/**
+ * JS-116: Interacting with the DOM - Traversing the DOM
+ * 
+ * Code challenge: Event Delegation
+ */
+let section = document.getElementsByTagName('section')[0];
+
+section.addEventListener('click', (e) => {
+  // Task 1: Add a condition that changes the background of the <input> elements only.
+  if( e.target.tagName === 'INPUT' ) {
+    e.target.style.backgroundColor = 'rgb(255, 255, 0)';
+  }
+});
