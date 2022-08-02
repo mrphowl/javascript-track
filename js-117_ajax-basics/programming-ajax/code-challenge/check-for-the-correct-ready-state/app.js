58afe156d3ed7c54c4d69dd8ce6654eb937bfe17
Paul Adraneda <pegadraneda@gmail.com>
2022-08-02 18:24:45 +0800

    Code challenge: Check for the correct ready state

---
+++ js-117_ajax-basics/programming-ajax/code-challenge/check-for-the-correct-ready-state/app.js
@@ -0,0 +1,21 @@
+/**
+ * JS-117: AJAX Basics: Programming AJAX
+ * 
+ * Code challenge: Check for the correct ready state
+ */
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+  // Task 1: Add a conditional statement inside the onreadystatechange event
+  // handler that tests to make sure the server has sent back its complete
+  // response.
+  // Task 2: Test to make sure the return status from the server is OK
+  if( xhr.readyState === 4 && xhr.status === 200 ) {
+    // Task 3: select the div with the ID of 'sidebar'.
+    const sidebar = document.getElementById('sidebar');
+    // Task 4: Use a DOM element's innerHTML property to set the HTML inside
+    // the sidebar div to the server's response.
+    sidebar.innerHTML = xhr.responseText;
+  }
+};
+xhr.open('GET', 'sidebar.html');
+xhr.send();
