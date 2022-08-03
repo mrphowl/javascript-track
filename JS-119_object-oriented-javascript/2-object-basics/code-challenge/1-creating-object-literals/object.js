a478470e494b5b54d8f8a2dbba2cdebb7b2c40bb
Paul Adraneda <pegadraneda@gmail.com>
2022-08-03 19:01:13 +0800

    Code challenge: Creating object literals

---
+++ JS-119_object-oriented-javascript/2-object-basics/code-challenge/1-creating-object-literals/object.js
@@ -0,0 +1,16 @@
+/**
+ * JS-119: Object-Oriented JavaScript
+ * 
+ * Code challenge: Creating object literals
+ */
+const player1 = {
+  // Task 1: Fill in the missing code for the object literal.
+  // To do this, inside the curly braces, create three key value pairs for
+  // properties name, color, and isTurn. The values for name and color should be
+  // any string value, and the value for isTurn should be true.
+  name: 'Elie',
+  color: 'Orange',
+  isTurn: true,
+  // Task 2: Add an empty method to the object literal called play().
+  play: function() {}
+}
