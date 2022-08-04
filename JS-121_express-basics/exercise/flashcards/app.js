d2f690c58390f70f06c4b8d371fe33d0baf7f4b0
Paul Adraneda <pegadraneda@gmail.com>
2022-08-04 19:56:52 +0800

    Add more routes.
    - Log a message to the console that the server is running
    - Return HTML instead of plain text

--- JS-121_express-basics/exercise/flashcards/app.js
+++ JS-121_express-basics/exercise/flashcards/app.js
@@ -5,10 +5,21 @@ const app = express();
 
 // The get() methods handles GET requests
 // Root route
-app.get('/', (request, response) => {
+app.get('/', (req, res) => {
   // the send() method send the string to the client
-  response.send('Welcome to Code and Theory!');
+  res.send(`<h1>Welcome to Code and Theory!</h1>
+  <a href="/hello" title="Hello!">Hello!</a>`);
+});
+
+// hello route
+app.get('/hello', (req, res) => {
+  // the send() method send the string to the client
+  res.send(`<h1>Hello, hello!</h1>
+  <a href="/" title="Go to the homepage.">Homepage</a>`);
 });
 
 // Make the app accessible in localhost at port 3000
-app.listen(3000);
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}.`);
+});
