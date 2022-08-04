3bdc9ff642e2678115cba42d350d19fe9f79b9c5
Paul Adraneda <pegadraneda@gmail.com>
2022-08-04 19:39:46 +0800

    Creating a route with Express
    - Create the root route
    - Install nodemon (doc: installing-nodemon.md)
    - Changed main script to app.js to wirk with nodemon

--- JS-121_express-basics/exercise/flashcards/app.js
+++ JS-121_express-basics/exercise/flashcards/app.js
@@ -3,5 +3,12 @@ const express = require('express');
 // Create an Express application
 const app = express();
 
+// The get() methods handles GET requests
+// Root route
+app.get('/', (request, response) => {
+  // the send() method send the string to the client
+  response.send('Welcome to Code and Theory!');
+});
+
 // Make the app accessible in localhost at port 3000
 app.listen(3000);
