cf728824a6785d37eac7d3ebd75beb0fc61e872f
Paul Adraneda <pegadraneda@gmail.com>
2022-08-08 14:43:46 +0800

    Using templates
    - Add a /card route
    - Render using the template file

--- JS-121_express-basics/1-getting-started-with-express/exercise/flashcards/app.js
+++ JS-121_express-basics/1-getting-started-with-express/exercise/flashcards/app.js
@@ -11,17 +11,20 @@ app.set('view engine', 'pug');
 // Root route
 app.get('/', (req, res) => {
   const data = {
-    title: 'I was once a cucumber'
+    title: 'Flash Cards'
   };
   // the send() method send the string to the client
   res.render('index', data);
 });
 
 // hello route
-app.get('/hello', (req, res) => {
+app.get('/card', (req, res) => {
+  const data = {
+    prompt : "Who is buried in Grant's tomb?",
+    hint: "Think about whose tomb it is."
+  };
   // the send() method send the string to the client
-  res.send(`<h1>Hello, hello!</h1>
-  <a href="/" title="Go to the homepage.">Homepage</a>`);
+  res.render('card', data);
 });
 
 // Make the app accessible in localhost at port 3000
