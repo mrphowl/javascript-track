05ba6514ab9ce0c05b9479899dae867484bd468a
Paul Adraneda <pegadraneda@gmail.com>
2022-08-18 22:09:26 +0800

    New routes: Get all quotes, get a single quote.

--- JS-121_express-basics/11-rest-apis-with-express/exercise/app.js
+++ JS-121_express-basics/11-rest-apis-with-express/exercise/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
+const { records } = require('./data.json');
 
-app.get('/greetings', (req, res) => {
-  res.json({greeting: "Hello, Express!"})
-});
+const getRecordById = id => records.find( record => record.id == id );
 
 // Send a GET request to /quotes to READ a list of quote.
+app.get('/quotes', (req, res) => {
+  res.json(records);
+});
 // Send a GET request to /quotes/:id to READ (view) a quote.
+app.get('/quotes/:id', (req, res) => {
+  const id = req.params.id;
+  const quote = getRecordById(id);
+  res.json(quote);
+});
 // Send a POST request to /quotes to CREATE new a quote.
 // Send a PUT request to /quotes/:id to UPDATE (edit) a quote.
 // Send a DELETE request to /quotes/:id to DELETE a quote.
