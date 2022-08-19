cf3ec33e4a18a1f1a04bb0f6cb2dfae9a9d3bcce
Paul Adraneda <pegadraneda@gmail.com>
2022-08-19 19:02:02 +0800

    Add a function to handle the async/await call for all routes.

--- JS-121_express-basics/11-rest-apis-with-express/exercise/app.js
+++ JS-121_express-basics/11-rest-apis-with-express/exercise/app.js
@@ -5,83 +5,74 @@ const records = require('./records');
 
 app.use(express.json());
 
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
 // Send a GET request to /quotes to READ a list of quote.
-app.get('/quotes', async (req, res, next) => {
-  try {
-    const quotes = await records.getQuotes();
-    res.json(quotes);
-  } catch (error) {
-    next(error);
-  }
-});
+app.get('/quotes', asyncHandler( async (req, res, next) => {
+  const quotes = await records.getQuotes();
+  res.json(quotes);
+}));
+
 // Send a GET request to /quotes/:id to READ (view) a quote.
-app.get('/quotes/:id', async (req, res, next) => {
-  try {
-	  const quote = await records.getQuote(req.params.id);
-    if( quote ) {
-	    res.json(quote);
-    } else {
-      res.status(404).json({ message: 'Not found.' });
-    }
-  } catch (error) {
-    next(error);
+app.get('/quotes/:id', asyncHandler( async (req, res, next) => {
+  const quote = await records.getQuote(req.params.id);
+  if( quote ) {
+    res.json(quote);
+  } else {
+    res.status(404).json({ message: 'Not found.' });
   }
-});
+}));
+
 // Send a POST request to /quotes to CREATE new a quote.
-app.post('/quotes', async (req, res, next) => {
+app.post('/quotes', asyncHandler( async (req, res, next) => {
   const { quote, author } = req.body;
-  try {
-    if( quote && author ) {
-      const record = await records.createQuote({ quote, author });
-      res.status(201).json(record);
-    } else {
-      res.status(400).json({ message: 'Quote and author required.'});
-    }
-  } catch (error) {
-    next(error);
+  if( quote && author ) {
+    const record = await records.createQuote({ quote, author });
+    res.status(201).json(record);
+  } else {
+    res.status(400).json({ message: 'Quote and author required.'});
   }
-});
+}));
+
 // Send a PUT request to /quotes/:id to UPDATE (edit) a quote.
-app.put('/quotes/:id', async (req, res, next) => {
+app.put('/quotes/:id', asyncHandler( async (req, res, next) => {
   const { id } = req.params;
   const { quote, author } = req.body;
-  try {
-	  const record = await records.getQuote(id);
-    if( record ) {
-      record.quote = quote;
-      record.author = author;
-      records.updateQuote(record);
-	    res.status(204).end();
-    } else {
-      res.status(404).json({ message: 'Quote not found.' });
-    }
-  } catch (error) {
-    next(error);
+  const record = await records.getQuote(id);
+  if( record ) {
+    record.quote = quote;
+    record.author = author;
+    records.updateQuote(record);
+    res.status(204).end();
+  } else {
+    res.status(404).json({ message: 'Quote not found.' });
   }
-});
+}));
+
 // Send a DELETE request to /quotes/:id to DELETE a quote.
-app.delete('/quotes/:id', async (req, res, next) => {
-  try {
-	  const record = await records.getQuote(req.params.id);
-	  if ( record ) {
-	    await records.deleteQuote(req.body);
-	    res.status(204).end();
-	  } else {
-	    res.status(404).json('Quote not found.');
-	  }
-  } catch (error) {
-    next(error);
+app.delete('/quotes/:id', asyncHandler( async (req, res, next) => {
+  const record = await records.getQuote(req.params.id);
+  if ( record ) {
+    await records.deleteQuote(req.body);
+    res.status(204).end();
+  } else {
+    res.status(404).json('Quote not found.');
   }
-});
+}));
+
 // Send a GET request to /quotes/quote/random to READ (view) a random quote.
-app.get('/quotes/quote/random', async (req, res, next) => {
-  try {
-	  const quote = await records.getRandomQuote();
-	  res.json(quote);
-  } catch (error) {
-    next(error);
-  }
-});
+app.get('/quotes/quote/random', asyncHandler( async (req, res, next) => {
+  const quote = await records.getRandomQuote();
+  res.json(quote);
+}));
 
 app.use( (req, res, next) => {
   const error = new Error('Not found');
